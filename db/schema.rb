2f657fd2d3e576d20cccfe47393b9a501323531d
E. Lynette Rayle <elr37@cornell.edu>
2019-09-10 09:23:00 -0400

    revert schema back to pre-PR 103 state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190816015611) do
+ActiveRecord::Schema.define(version: 20190816015610) do
 
   create_table "performance_history", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "dt_stamp"
